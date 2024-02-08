6d8a6873fdc287d6ea7bbc47e8dbd02324ae45c8
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-08 12:44:37 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -4,24 +4,18 @@ from collections import deque
 read = sys.stdin.readline
 def backtracking(nums, count):
     if count == m:
-        for i in range(m):
-            if i != m-1:
-                print(nums[i], end=' ')
-            else:
-                print(nums[i])
+        print(' '.join(map(str, nums)))
         return
     for i in range(1, n+1):
-        if nums.count(i) != 0:
+        if i in nums:
             continue
         nums.append(i)
         backtracking(nums, count+1)
         nums.pop()
 
 
-
 n, m = map(int, read().split())
-
-nums = deque()
+nums = []
 for i in range(1, n+1):
     nums.append(i)
     backtracking(nums, 1)
