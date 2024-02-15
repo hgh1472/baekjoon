d3088c5da388fa159b7afdcbdc01d2e3ec956220
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-15 12:16:26 +0900

    [Gold IV] Title: 단어 수학, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold IV/1339. 단어 수학/단어 수학.py
+++ 백준/Gold IV/1339. 단어 수학/단어 수학.py
@@ -3,33 +3,19 @@ import sys
 read = sys.stdin.readline
 
 n = int(read())
-info = {10 ** 0: [], 10 ** 1: [], 10 ** 2: [], 10 ** 3: [], 10 ** 4: [],
-        10 ** 5: [], 10 ** 6: [], 10 ** 7: []}
-
+info = [0] * 26
 for i in range(n):
     alphas = read().rstrip()
-    for j in range(len(alphas)-1, -1, -1):
-        info[10 ** (len(alphas) - j - 1)].append(alphas[j])
+    for j in range(len(alphas)):
+        info[ord(alphas[len(alphas) - 1 - j]) - ord('A')] += 10 ** j
 
-dic = {}
-for i in range(26):
-    dic[chr(ord('A') + i)] = 0
-num = [0] * 26
-for i in range(len(info)):
-    for alpha in info[10 ** i]:
-        dic[alpha] += 10 ** i
-
-weight = 9
-
-sorted_dic = sorted(dic.items(), key = lambda x:x[1], reverse=True)
-for alpha in sorted_dic:
-    if alpha[1] != 0:
-        num[ord(alpha[0]) - ord('A')] = weight
-        weight -= 1
-
+info.sort(reverse=True)
 result = 0
-for i in range(len(info)):
-    for alpha in info[10 ** i]:
-        result += num[ord(alpha) - ord('A')] * 10 ** i
+num = 9
+for i in info:
+    if i == 0:
+        break
+    result += num * i
+    num -= 1
 
 print(result)
