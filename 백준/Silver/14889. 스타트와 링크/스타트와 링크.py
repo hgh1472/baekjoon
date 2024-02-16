0a6a35c7ac6080549e4eb0eb345d3849f52d6497
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-16 18:11:00 +0900

    [Silver I] Title: 스타트와 링크, Time: 5388 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,32 @@
+import sys
+from itertools import combinations
+
+read = sys.stdin.readline
+
+n = int(read())
+players = []
+for i in range(n):
+    players.append(i)
+
+s = []
+diff = 40000
+for i in range(n):
+    s.append(list(map(int, read().split())))
+for com in combinations(players, n // 2):
+    a = 0
+    b = 0
+    for pair in combinations(com, 2):
+        a += s[pair[0]][pair[1]]
+        a += s[pair[1]][pair[0]]
+    temp = []
+    for i in range(n):
+        if i in com:
+            continue
+        temp.append(i)
+    for pair in combinations(temp, 2):
+        b += s[pair[0]][pair[1]]
+        b += s[pair[1]][pair[0]]
+    if abs(a - b) < diff:
+        diff = abs(a - b)
+
+print(diff)
