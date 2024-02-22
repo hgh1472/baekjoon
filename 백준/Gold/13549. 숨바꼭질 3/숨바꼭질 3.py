188fd6adb60247fe587959fac9c7ef0bb2fa51ef
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-22 10:57:00 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,29 +1,21 @@
 import sys
-import heapq
 
 read = sys.stdin.readline
 
 n, k = map(int, read().split())
 
 
-def sol():
-    if n >= k:
-        print(n - k)
-        return
-    visited = [sys.maxsize] * 100001
-    l = []
-    heapq.heappush(l, (0, n))
-    while l:
-        cost, pos = heapq.heappop(l)
-        visited[pos] = cost
-        if pos == k:
-            print(cost)
-            return
-        if pos + 1 <= k and visited[pos + 1] > cost + 1:
-            heapq.heappush(l, (cost + 1, pos+1))
-        if 0 <= pos - 1 <= k + 1 and visited[pos - 1] > cost + 1:
-            heapq.heappush(l, (cost + 1, pos-1))
-        if 0 < pos * 2 <= k + 1 and visited[pos * 2] > cost:
-            heapq.heappush(l, (cost, 2 * pos))
+def solution(n, k):
+	if n >= k:
+		return n - k
+	elif k == 1:
+		return 1
+	elif k % 2:
+		return 1 + min(solution(n, k - 1), solution(n, k + 1))
+	else:
+		return min(solution(n, k // 2), k - n)
 
-sol()
+
+result = solution(n, k)
+print(result)
+
