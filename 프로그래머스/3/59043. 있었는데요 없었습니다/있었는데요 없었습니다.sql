1fc47c632fae2c4c11a364363df2e2fd56864f4f
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-24 13:12:35 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT ins.ANIMAL_ID, ins.NAME from ANIMAL_INS as ins
+join ANIMAL_OUTS as outs on ins.ANIMAL_ID = outs.ANIMAL_ID
+where ins.DATETIME >= outs.DATETIME
+order by ins.DATETIME
