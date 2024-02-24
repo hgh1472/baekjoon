c7295dfb3840c94e983b7d6c9b8c5e12dd174b8a
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-24 12:56:44 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT outs.ANIMAL_ID, outs.NAME from ANIMAL_OUTS as outs
+left join ANIMAL_INS as ins on outs.ANIMAL_ID = ins.ANIMAL_ID
+where ins.ANIMAL_ID is null
+order by ANIMAL_ID
