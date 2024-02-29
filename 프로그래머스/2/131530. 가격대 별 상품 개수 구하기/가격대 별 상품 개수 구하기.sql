cc347acd91e545d8feedafe78086cf43198e659a
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-02-29 13:11:42 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,3 +1,3 @@
-select PRICE_GROUP, COUNT(*) as PRODUCTS from (select truncate(PRICE, -4) as PRICE_GROUP from PRODUCT as a) as b
+select truncate(PRICE, -4) as PRICE_GROUP, COUNT(*) as PRODUCTS from PRODUCT
 group by PRICE_GROUP
 order by PRICE_GROUP
