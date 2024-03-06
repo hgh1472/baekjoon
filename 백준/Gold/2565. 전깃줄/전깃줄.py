ad3db4b768a3232836c223b471aa40f441f42217
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-07 00:07:46 +0900

    [Gold V] Title: 전깃줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -0,0 +1,28 @@
+# max(가장 긴 증가하는 수열, 가장 긴 감소하는 수열)
+import sys
+
+input = sys.stdin.readline
+
+
+n = int(input())
+lines = []
+for i in range(n):
+    a, b = map(int, input().split())
+    lines.append([a, b])
+
+lines.sort()
+sorted_lines = []
+for i in range(n):
+    sorted_lines.append(lines[i][1])
+# dp는 i번째까지 중에서 value[i]보다 작은 것 중 dp[i]값이 가장 큰 것을 선택한다.
+#dp[i]는 dp[i]까지 가능한 최대 길이의 증가하는 수열의 길이이다.
+inc_dp = [1] * n
+
+# 가장 긴 증가하는 수열 구하기
+for i in range(n):
+    local = 0
+    for j in range(i):
+        if sorted_lines[j] < sorted_lines[i]:
+            local = max(local, inc_dp[j])
+        inc_dp[i] = local + 1
+print(n - max(inc_dp))
