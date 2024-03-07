a8aded209b6f3938ef82138fd8014fd74b20fe2a
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-08 00:39:02 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 876 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -5,31 +5,20 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [[] for _ in range(n+1)]
-dp[1] = [[1, 3]]
-for i in range(2, n + 1):
-    for a, b in dp[i-1]:
-        if a == 2:
-            a = 3
-        elif a == 3:
-            a = 2
-        if b == 2:
-            b = 3
-        elif b == 3:
-            b = 2
-        dp[i].append([a, b])
-    dp[i].append([1, 3])
-    for a, b in dp[i- 1]:
-        if a == 2:
-            a = 1
-        elif a == 1:
-            a = 2
-        if b == 2:
-            b = 1
-        elif b == 1:
-            b = 2
-        dp[i].append([a, b])
 
-print(len(dp[n]))
-for a, b in dp[n]:
-    print(a, b)
+def solve(n, x, y):
+    if n == 1:
+        print(x, y)
+        return
+
+    solve(n - 1, x, 6 - x - y)
+    print(x, y)
+    solve(n-1,6 - x - y, y)
+
+
+dp = [0] * (n + 1)
+dp[1] = 1
+for i in range(2, n + 1):
+    dp[i] = 2*dp[i - 1] + 1
+print(dp[n])
+solve(n, 1, 3)
