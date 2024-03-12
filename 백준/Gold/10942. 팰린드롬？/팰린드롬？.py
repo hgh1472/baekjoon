177589c388152ff03aaa68ca59712a2ceeee9d1e
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-12 15:42:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2256 ms, Memory: 46300 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -7,21 +7,10 @@ n = int(input())
 numbers = list(map(int, input().split()))
 m = int(input())
 
-same_range = []
-start, end = 0, 0
-for i in range(1, n):
-    if numbers[start] == numbers[i]:
-        end = i
-    else:
-        if end != start:
-            same_range.append([start, end])
-        start = i
-if end != start:
-    same_range.append([start, end])
-# dp의 첫번째 인덱스는 numbers에서 기준이되는 숫자의 인덱스
-# dp의 두번째 인덱스는 기준 인덱스에서 멀어지는 범위를 말한다. 즉, dp[3][2]는 2 1 3 1 2이다.
 # numbers의 인덱스 중앙값을 기준으로 최대 + n // 2, - n // 2까지의 인덱스를 가질 수 있다.
 max_dis = n // 2
+# dp의 첫번째 인덱스는 numbers에서 기준이되는 숫자의 인덱스
+# dp의 두번째 인덱스는 기준 인덱스에서 멀어지는 범위를 말한다. 즉, dp[3][2]는 2 1 3 1 2이다.
 dp = [[0] * (max_dis + 1) for _ in range(n)]
 for i in range(n):
     # 자기 자신만 가지는 수열은 팰린드롬이다.
@@ -38,6 +27,7 @@ for i in range(n):
                 dp[i][j] = 1
         else:  # 범위를 넓혀갈 때 팰린드롬이 아닌 수가 나오면 그 수열을 포함한 수열은 팰린드롬이 아니다.
             break
+
 
 for i in range(m):
     s, e = map(int, input().split())
