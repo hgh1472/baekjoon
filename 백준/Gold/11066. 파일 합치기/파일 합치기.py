cfd7fe3768a303eb8b92254652070967cb3d6c9b
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-16 10:13:23 +0900

    [Gold III] Title: 파일 합치기, Time: 3012 ms, Memory: 116308 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -7,14 +7,10 @@ t = int(input())
 for _ in range(t):
     k = int(input())
     files = list(map(int, input().split()))
-    files_sum = [[0] * k for _ in range(k)]
-    for i in range(k):
-        files_sum[i][i] = files[i]
-        if i != k - 1:
-            files_sum[i][i + 1] = files[i] + files[i + 1]
-    for i in range(k):
-        for j in range(i + 2, k):
-            files_sum[i][j] = files_sum[i][j-1] + files[j]
+    files_sum = [files[0]]
+    for i in range(1, k):
+        files_sum.append(files_sum[i-1] + files[i])
+
 
     dp = [[0] * k for _ in range(k)]
     for dif in range(1, k):
@@ -23,7 +19,11 @@ for _ in range(t):
                 break
             dp[start][start+dif] = sys.maxsize
             for t in range(start, start+dif):
+                if start != 0:
+                    temp = files_sum[start + dif] - files_sum[start - 1]
+                else:
+                    temp = files_sum[start+dif]
                 dp[start][start+dif] = min(dp[start][start+dif],
-                                           dp[start][t]+dp[t+1][start+dif] + files_sum[start][t] + files_sum[t+1][start+dif])
+                                           dp[start][t]+dp[t+1][start+dif] + temp)
     print(dp[0][k-1])
 
