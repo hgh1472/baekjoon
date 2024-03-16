cd66a6d685d3e1c83cfe6e594d6cc80cbc8d3516
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-16 10:07:30 +0900

    [Gold III] Title: 파일 합치기, Time: 3528 ms, Memory: 123828 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.py
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.py
@@ -9,8 +9,12 @@ for _ in range(t):
     files = list(map(int, input().split()))
     files_sum = [[0] * k for _ in range(k)]
     for i in range(k):
-        for j in range(k):
-            files_sum[i][j] = sum(files[i:j+1])
+        files_sum[i][i] = files[i]
+        if i != k - 1:
+            files_sum[i][i + 1] = files[i] + files[i + 1]
+    for i in range(k):
+        for j in range(i + 2, k):
+            files_sum[i][j] = files_sum[i][j-1] + files[j]
 
     dp = [[0] * k for _ in range(k)]
     for dif in range(1, k):
