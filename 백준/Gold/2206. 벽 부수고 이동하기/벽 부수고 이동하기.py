c235d88bf83ddff883a0e8fd8d3d456eda7973a8
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-19 16:45:37 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3476 ms, Memory: 152580 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -7,10 +7,10 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 
 info = []
-visited = [[[0, 0] for _ in range(m)] for _ in range(n)]
+visited = [[[False, False] for _ in range(m)] for _ in range(n)]
 for i in range(n):
     info.append(list(input().rstrip()))
-visited[0][0][0] = visited[0][0][1] = 1
+visited[0][0][0] = visited[0][0][1] = True
 
 
 def bfs():
@@ -18,32 +18,27 @@ def bfs():
     q.append([0, 0, 1, 0])
     while q:
         x, y, distance, broke = q.popleft()
+        if x == n-1 and y == m-1:
+            print(distance)
+            return
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if 0 <= nx < n and 0 <= ny < m:
                 if broke == 1 and info[nx][ny] == '0':
-                    if visited[nx][ny][broke] == 0:
-                        visited[nx][ny][broke] = distance + 1
+                    if not visited[nx][ny][broke]:
+                        visited[nx][ny][broke] = True
                         q.append([nx, ny, distance + 1, broke])
                 elif broke == 0:
                     if info[nx][ny] == '0':
-                        if visited[nx][ny][broke] == 0:
-                            visited[nx][ny][broke] = distance + 1
+                        if not visited[nx][ny][broke]:
+                            visited[nx][ny][broke] = True
                             q.append([nx, ny, distance + 1, broke])
                     else:
-                        if visited[nx][ny][1] == 0:
-                            visited[nx][ny][1] = distance + 1
+                        if not visited[nx][ny][1]:
+                            visited[nx][ny][1] = True
                             q.append([nx, ny, distance + 1, 1])
+    print(-1)
 
 
 bfs()
-
-if visited[-1][-1][0] == 0 and visited[-1][-1][1] == 0:
-    print(-1)
-elif visited[-1][-1][0] == 0:
-    print(visited[-1][-1][1])
-elif visited[-1][-1][1] == 0:
-    print(visited[-1][-1][0])
-else:
-    print(min(visited[-1][-1][0], visited[-1][-1][1]))
