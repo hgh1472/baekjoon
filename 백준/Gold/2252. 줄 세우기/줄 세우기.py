5872c809c724f9c918371850638c3188b4841659
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-22 10:36:05 +0900

    [Gold III] Title: 줄 세우기, Time: 168 ms, Memory: 40504 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,29 +1,27 @@
 import sys
-import heapq
-from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
 
 n, m = map(int, input().split())
-in_edges = [0] * (n+1)
-out_edges = [[] for _ in range(n+1)]
+in_edges = [[] for _ in range(n+1)]
 visited = [False] * (n + 1)
 for i in range(m):
     a, b = map(int, input().split())
-    out_edges[a].append(b)
-    in_edges[b] += 1
+    in_edges[b].append(a)
 
-q = []
-for i in range(1, n+1):
-    heapq.heappush(q, (in_edges[i], i))
+lines = []
 
-while q:
-    count, num = heapq.heappop(q)
+
+def dfs(num):
     if visited[num]:
-        continue
-    print(num, end=' ')
+        return
     visited[num] = True
-    for i in out_edges[num]:
-        in_edges[i] -= 1
-        heapq.heappush(q, (in_edges[i], i))
+    for i in in_edges[num]:
+        dfs(i)
+    lines.append(str(num))
+
+
+for i in range(1, n+1):
+    dfs(i)
+print(" ".join(lines))
