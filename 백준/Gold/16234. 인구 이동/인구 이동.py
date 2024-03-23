8d48806cd2dc093424fad80d4998736ab8319034
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-23 15:23:01 +0900

    [Gold IV] Title: 인구 이동, Time: 240 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.py
+++ 백준/Gold/16234. 인구 이동/인구 이동.py
@@ -1,50 +1,44 @@
 import sys
 from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def bfs(x, y):
-    q = deque()
-    q.append([x, y])
-    visited[x][y] = day
-    pos = [[x, y]]
-    populations = nations[x][y]
-    while q:
-        a, b = q.popleft()
+def bfs(tx, ty):
+    visited[tx][ty] = day
+    stack = [[tx, ty]]
+    populations = nations[tx][ty]
+    for a, b in stack:
         for i in range(4):
             nx = a + dx[i]
             ny = b + dy[i]
             if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != day:
-                if l <= abs(nations[a][b] - nations[nx][ny]) <= r:
-                    pos.append([nx, ny])
+                if L <= abs(nations[a][b] - nations[nx][ny]) <= R:
                     visited[nx][ny] = day
                     populations += nations[nx][ny]
-                    q.append([nx, ny])
-    count = len(pos)
-    for a, b in pos:
-        nations[a][b] = populations // count
-    return count
+                    stack.append([nx, ny])
+    if len(stack) > 1:
+        avg = populations // len(stack)
+        for a, b in stack:
+            nations[a][b] = avg
+            pos.append([a, b])
 
 
-n, l, r = map(int, input().split())
+n, L, R = map(int, input().split())
 nations = []
 for i in range(n):
     nations.append(list(map(int, input().split())))
 
-day = -1
-flag = 1
+pos = deque([x, y] for y in range(n) for x in range(y % 2, n, 2))
+day = 0
 visited = [[-1] * n for _ in range(n)]
-while flag == 1:
+while pos:
+    for _ in range(len(pos)):
+        x, y = pos.popleft()
+        if visited[x][y] < day:
+            bfs(x, y)
     day += 1
-    flag = 0
-    for i in range(n):
-        for j in range(n):
-            if visited[i][j] != day:
-                change = bfs(i, j)
-                if change >= 2:
-                    flag = 1
 
-print(day)
+
+print(day-1)
