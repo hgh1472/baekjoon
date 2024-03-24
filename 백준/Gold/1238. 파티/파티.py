8a9ed04bc12fee7930996fd4f778a0d945d9e9f5
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-24 19:08:32 +0900

    [Gold III] Title: 파티, Time: 60 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -2,29 +2,34 @@ import sys
 import heapq
 input = sys.stdin.readline
 
+
+def dijkstra(start, bridges, distance):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dis, node = heapq.heappop(q)
+        if distance[node] < dis:
+            continue
+        for end, time in bridges[node]:
+            if distance[end] > dis + time:
+                distance[end] = dis + time
+                heapq.heappush(q, (distance[end], end))
+
+
 N, M, X = map(int, input().split())
-bridge = [[] for _ in range(N+1)]
+bridge_to_x = [[] for _ in range(N+1)]
+bridge_from_x = [[] for _ in range(N+1)]
+distance_to_x = [sys.maxsize] * (N+1)
+distance_from_x = [sys.maxsize] * (N+1)
+
 for i in range(M):
     s, e, t = map(int, input().split())
-    bridge[s].append([e, t])
+    bridge_to_x[s].append([e, t])
+    bridge_from_x[e].append([s, t])
 
-result = [0] * (N+1)
-for i in range(1, N+1):
-    distance = [sys.maxsize] * (N+1)
-    distance[i] = 0
-    q = []
-    heapq.heappush(q, (0, i))
-    while q:
-        d, a = heapq.heappop(q)
-        if distance[a] < d:  # 현재보다 더 짧은 간선이 이미 반영된 경우
-            continue
-        for end, time in bridge[a]:
-            if distance[a] + time < distance[end]:
-                distance[end] = distance[a] + time
-                heapq.heappush(q, (distance[end], end))
-    if i == X:
-        for j in range(1, N+1):
-            result[j] += distance[j]
-    else:
-        result[i] += distance[X]
+dijkstra(X, bridge_to_x, distance_to_x)
+dijkstra(X, bridge_from_x, distance_from_x)
+
+result = [distance_to_x[i] + distance_from_x[i] for i in range(1, N+1)]
 print(max(result))
