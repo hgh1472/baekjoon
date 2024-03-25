0d540e300157243b34d83798f31ffe2150b1ed8b
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-03-25 14:02:27 +0900

    [Gold IV] Title: 이분 그래프, Time: 2516 ms, Memory: 52552 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,39 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+def bfs(start, g):
+    visited[start] = g
+    q = deque()
+    q.append(start)
+    while q:
+        node = q.popleft()
+        for c in edge[node]:
+            if visited[c] == 0:
+                visited[c] = (visited[node] + 1) % 2
+                q.append(c)
+            else:
+                if visited[c] == visited[node]:
+                    return False
+    return True
+
+
+k = int(input())
+for _ in range(k):
+    v, e = map(int, input().split())
+    edge = [[]for _ in range(v+1)]
+    for i in range(e):
+        a, b = map(int, input().split())
+        edge[a].append(b)
+        edge[b].append(a)
+    visited = [0] * (v+1)
+    divisible = True
+    for i in range(1, v+1):
+        divisible = bfs(i, visited[i])
+        if not divisible:
+           break
+    if divisible:
+        print("YES")
+    else:
+        print("NO")
