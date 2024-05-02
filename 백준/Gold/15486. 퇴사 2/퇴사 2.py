c234fa0f23092ad8a14471b4019550b465104204
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-05-02 16:02:36 +0900

    [Gold V] Title: 퇴사 2, Time: 2468 ms, Memory: 96084 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -2,15 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+income = [0] * (n + 1)
 schedule = []
 for i in range(n):
-    schedule.append(list(map(int, input().split())))
-
-income = [0] * (n + 1)
-max_income = 0
-for i in range(n):
+    day, pay = map(int, input().split())
     income[i] = max(income[i-1], income[i])
-    if i + schedule[i][0] <= n:
-        income[i + schedule[i][0]] = max(income[i] + schedule[i][1], income[i + schedule[i][0]])
+    if i + day <= n:
+        income[i + day] = max(income[i] + pay, income[i + day])
 
 print(max(income[n-1], income[n]))
