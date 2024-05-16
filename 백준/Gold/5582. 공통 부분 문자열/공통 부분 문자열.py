aa4834371a38d32b6b01dbb5c362d9ac578b7685
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-05-16 17:40:31 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 908 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -6,17 +6,14 @@ b = input().rstrip()
 len_a = len(a)
 len_b = len(b)
 
-dp = [0] * (len_a + 1)
-tmp = [0] * (len_a + 1)
 longest = 0
+start, end = 0, 1
+while end <= len_a:
+    if a[start:end] in b:
+        end += 1
+    else:
+        start += 1
+    longest = max(longest, end - start)
 
-for i in range(len_b):
-    tmp = dp.copy()
-    for j in range(len_a):
-        if b[i] == a[j]:
-            dp[j + 1] = tmp[j] + 1
-        else:
-            dp[j + 1] = 0
-    longest = max(longest, max(dp))
+print(longest - 1)
 
-print(longest)
