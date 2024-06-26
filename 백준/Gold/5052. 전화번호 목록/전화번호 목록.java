1c0d79bbbf3d196746602f8712d37c83bce5e9e1
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-06-26 10:52:19 +0900

    [Gold IV] Title: 전화번호 목록, Time: 388 ms, Memory: 105152 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -8,9 +8,10 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            Trie[] init = new Trie[10];
+            Trie init = new Trie('s');
             List<String> numbers = new ArrayList<>();
             for (int j = 0; j < n; j++) {
                 numbers.add(br.readLine());
@@ -18,33 +19,26 @@ class Main {
             boolean isDuplicated = false;
             for (int j = 0; j < n; j++) {
                 String number = numbers.get(j);
-                char num = number.charAt(0);
-                if (init[num - '0'] == null) {
-                    init[num - '0'] = new Trie(num);
-                }
-                Trie curr = init[num - '0'];
-                if (curr.isLast) {
-                    isDuplicated = true;
-                    break;
-                }
-                if (number.length() == 1)
-                    curr.isLast = true;
-                for (int k = 1; k < number.length(); k++) {
-                    num = number.charAt(k);
+                Trie curr = init;
+                for (int k = 0; k < number.length(); k++) {
+                    char num = number.charAt(k);
                     if (curr.tries[num - '0'] == null) {
                         curr.tries[num - '0'] = new Trie(num);
                     }
-                    else {
-                        if (k == number.length() - 1) {
-                            isDuplicated = true;
-                            break;
-                        }
+                    else if (curr.tries[num - '0'] != null && k == number.length() - 1) {
+                        // 이미 존재하는 Trie이고 내가 마지막이면, 일관성 X
+                        isDuplicated = true;
+                        break;
                     }
                     curr = curr.tries[num - '0'];
+                    
+                    // 지금까지 숫자가 이미 존재하는 번호면 일관성 X
                     if (curr.isLast == true) {
                         isDuplicated = true;
                         break;
                     }
+                    
+                    // 마지막 숫자면 하나의 번호로 반영
                     if (k == number.length() - 1) {
                         curr.isLast = true;
                     }
