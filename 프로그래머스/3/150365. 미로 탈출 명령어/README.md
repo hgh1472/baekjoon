1e35854dd15609f4fbf6ce682b18b8a5839c350b
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-07-10 13:45:10 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/README.md
+++ 프로그래머스/3/150365. 미로 탈출 명령어/README.md
@@ -1,10 +1,10 @@
 # [level 3] 미로 탈출 명령어 - 150365 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150365) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.08 ms
+메모리: 72.9 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 12:48:13
+2024년 07월 10일 13:45:08
 
 ### 문제 설명
 
--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,100 +1,36 @@
 class Solution {
-    static StringBuilder sb;
-    static int down = 0, left = 1, right = 2, up = 3;
-    static int[] command;
-    static int limit, a, b, dr, dc;
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
         if (Math.abs(x - r) + Math.abs(y - c) > k || (k - (Math.abs(x - r) + Math.abs(y - c))) % 2 == 1)
             return "impossible";
-        sb = new StringBuilder();
-        limit = k;
-        a = n;
-        b = m;
-        dr = r;
-        dc = c;
-        command = new int[4];
-        if (r - x < 0) // 도착점이 더 위에 있음
-            command[up] += -(r-x);
-        else
-            command[down] += r-x;
-        if (c - y < 0) // 도착점이 더 왼쪽에 있음
-            command[left] += -(c-y);
-        else
-            command[right] += c-y;
-        
-        dfs(x, y, Math.abs(r-x) + Math.abs(c-y), 0);
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        while (x < n && count + Math.abs(x - r) + Math.abs(y - c) + 2 <= k) {
+            x++;
+            sb.append("d");
+            count++;
+        }
+        while (y > 1 &&  count + Math.abs(x - r) + Math.abs(y - c) + 2 <= k) {
+            y--;
+            count++;
+            sb.append("l");
+        }
+        while (count + Math.abs(x - r) + Math.abs(y - c) + 2 <= k) {
+            count += 2;
+            sb.append("rl");
+        }
+        if (x - r < 0) {
+            sb.append("d".repeat(Math.abs(x-r)));
+        }
+        if (c - y < 0) {
+            sb.append("l".repeat(Math.abs(c-y)));
+        }
+        if (c - y > 0) {
+            sb.append("r".repeat(c-y));
+        }
+        if (x - r > 0) {
+            sb.append("u".repeat(x-r));
+        }
         return sb.toString();
     }
-    
-    // 플러스하는 것은 무조건 원점으로 돌아와야 한다.
-    public void dfs(int nx, int ny, int count, int k) {
-        int distance = Math.abs(dr - nx) + Math.abs(dc-ny);
-        // down
-        if (nx < a) {
-            if (command[down] > 0) {
-                command[down] -= 1;
-                sb.append("d");
-                dfs(nx+1, ny, count - 1,k + 1);
-                return;
-            }
-            if (count + k + 2 <= limit) {
-                command[down]+=1;
-                command[up]+=1;
-                dfs(nx, ny, count + 2,k);
-                return;
-            }
-        }
-        
-        // left
-        if (ny > 1) {
-            if (command[left] > 0) {
-                command[left]-=1;
-                sb.append("l");
-                dfs(nx, ny-1, count-1, k+1);
-                return;
-            }
-            if (count + k + 2 <= limit) {
-                command[left]+=1;
-                command[right]+=1;
-                dfs(nx,ny, count + 2, k);
-                return;
-            }
-        }
-        
-        // right
-        if (ny < b) {
-            if (command[right] > 0) {
-                command[right]-=1;
-                sb.append("r");
-                dfs(nx, ny+1, count-1, k+1);
-                return;
-            }
-            if (count + k + 2 <= limit) {
-                command[right]+=1;
-                command[left]+=1;
-                dfs(nx, ny, count + 2, k);
-                return;
-            }
-        }
-        
-        // up
-        if (nx > 1) {
-            if (command[up] > 0) {
-                command[up]-=1;
-                sb.append("u");
-                dfs(nx - 1, ny, count-1, k+1);
-                return;
-            }
-            if (count + k + 2 <= limit) {
-                command[up]+=1;
-                command[down]+=1;
-                dfs(nx, ny, count + 2, k);
-                return;
-            }
-        }
-    }
 }
-// 현재 위치에서 확인
-//  밑으로 내려갈 수 있나? => 남은 횟수와 현재 위치가 범위내에 있는지
-//      올라가는 경우까지 카운트한다.
-//  
+
