64dec71173f558cfd41ba9969e549ea42c8a1f16
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-12-12 18:41:30 +0900

    [level 3] Title: 주사위 고르기, Time: 651.89 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,104 +1,93 @@
 import java.util.*;
 class Solution {
     static int n;
-    static List<Integer> first;
-    static List<Integer> second;
-    static int[][] dices;
-    static int combCount;
-    static int[][] comb;
+    static int[] com;
+    static int[][] officialDice;
+    static int winCount = 0;
+    static int[] answerComb;
 
     public static int[] solution(int[][] dice) {
-        int[] answer = new int[n / 2];
         n = dice.length;
-        dices = dice;
-        getComb(n);
-        first = new ArrayList<>();
-        second = new ArrayList<>();
-        int maxCount = 0;
-        for (int i = 0; i < combCount; i++) {
-            first.clear();
-            second.clear();
-            boolean[] visited = new boolean[n + 1];
-            int[] a = comb[i];
-            for (int j = 0; j < a.length; j++)
-                visited[a[j]] = true;
-            calculate(a, 0, 0, first);
-
-            int[] b = new int[n / 2];
-            int bIdx = 0;
-            for (int j = 1; j <= n; j++) {
-                if (!visited[j])
-                    b[bIdx++] = j;
-            }
-            calculate(b, 0, 0, second);
-            
-            first.sort(Comparator.naturalOrder());
-            second.sort(Comparator.naturalOrder());
-
-            int winCount = calculateWinner(first, second);
-            if (maxCount < winCount) {
-                maxCount = winCount;
-                answer = a.clone();
-            }
-        }
-        return answer;
+        com = new int[n/2];
+        answerComb = new int[n/2];
+        officialDice = dice;
+        getCombination(0, 0);
+        return answerComb;
     }
 
-    public static void getComb(int n) {
-        int combLength = getFactorial(n) / (getFactorial(n / 2) * getFactorial(n / 2));
-        comb = new int[combLength][n / 2];
-        combCount = 0;
-        int[] pick = new int[n / 2];
-        for (int i = 1; i <= n / 2 + 1; i++) {
-            pick[0] = i;
-            makeComb(pick, 1);
-        }
-    }
-
-    public static void makeComb(int[] pick, int count) {
-        if (count == n / 2) {
-            comb[combCount] = pick.clone();
-            combCount++;
+    public static void getCombination(int depth, int idx) {
+        if (depth == n / 2) {
+            int win = getWinningCount();
+            if (winCount < win) {
+                winCount = win;
+                for (int i = 0; i < n/ 2; i++) {
+                    answerComb[i] = com[i] + 1;
+                }
+            }
             return;
         }
-        for (int i = pick[count - 1] + 1; i <= n; i++) {
-            pick[count] = i;
-            makeComb(pick, count + 1);
+        for (int i = idx; i < n; i++) {
+            com[depth] = i;
+            getCombination(depth + 1, i+ 1);
+            /**
+             * 0 1/ 0 2/0 3
+             *
+             */
         }
     }
 
-    public static void calculate(int[] a, int count, int sum, List<Integer> nums) {
-        if (count == n / 2) {
-            nums.add(sum);
+    public static int getWinningCount() {
+        List<Integer> listA = new ArrayList<>();
+        List<Integer> listB = new ArrayList<>();
+        int[][] comA = new int[n/2][6];
+        int[][] comB = new int[n/2][6];
+
+        int a = 0;
+        int b  = 0;
+        for (int i = 0; i < n; i++) {
+            boolean isContain = false;
+            for (int j = 0; j < n/2; j++) {
+                if (com[j] == i) {
+                    isContain = true;
+                    break;
+                }
+            }
+            if (isContain) {
+                comA[a++] = officialDice[i];
+            } else {
+                comB[b++] = officialDice[i];
+            }
+        }
+
+        makeList(listA, 0, 0, comA);
+        makeList(listB, 0, 0, comB);
+
+        Collections.sort(listB);
+        int count = 0;
+        for (int i = 0; i < listA.size(); i++) {
+            int left = 0;
+            int right = listB.size();
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (listB.get(mid) < listA.get(i)) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            count += right;
+        }
+        return count;
+    }
+
+    public static void makeList(List<Integer> list, int depth, int sum, int[][] dice) {
+        if (depth == n / 2) {
+            list.add(sum);
             return;
         }
         for (int i = 0; i < 6; i++) {
-            calculate(a, count + 1, sum + dices[a[count] - 1][i], nums);
+            makeList(list, depth + 1, sum + dice[depth][i], dice);
         }
     }
-    public static int getFactorial(int n) {
-        int result = 1;
-        for (int i = 1; i <= n; i++)
-            result *= i;
-        return result;
-    }
 
-    public static int calculateWinner(List<Integer> a, List<Integer> b) {
-        int winCount = 0;
-        int firstIdx = 0;
-        int secondIdx = 0;
-
-        while (firstIdx < a.size() && secondIdx < b.size()) {
-            if (a.get(firstIdx) > b.get(secondIdx)) {
-                secondIdx++;
-            }
-            else {
-                winCount += secondIdx;
-                firstIdx++;
-            }
-        }
-        if (firstIdx != a.size())
-            winCount += (a.size() - firstIdx) * a.size();
-        return winCount;
-    }
 }
