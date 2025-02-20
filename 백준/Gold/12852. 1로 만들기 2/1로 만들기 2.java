a56a362eb040264331207f15f83ae019b475bb4d
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-02-21 00:46:06 +0900

    [Gold V] Title: 1로 만들기 2, Time: 108 ms, Memory: 19176 KB -BaekjoonHub

--- 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
+++ 백준/Gold/12852. 1로 만들기 2/1로 만들기 2.java
@@ -4,56 +4,63 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
+    static int[] dp;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         boolean[] visited = new boolean[n + 1];
+        dp = new int[n + 1];
 
         PriorityQueue<Node> q = new PriorityQueue<>();
-        q.add(new Node(0, new ArrayList<>(), n));
+        q.add(new Node(0, n));
         visited[n] = true;
         while (!q.isEmpty()) {
             Node node = q.poll();
             if (node.num == 1) {
-                System.out.println(node.history.size());
+                Stack<Integer> stack = new Stack<>();
+                dfs(stack, 1);
+                System.out.println(stack.size() - 1);
                 StringBuilder sb = new StringBuilder();
-                for (Integer i : node.history) {
-                    sb.append(i).append(" ");
+                while (!stack.isEmpty()) {
+                    sb.append(stack.pop()).append(" ");
                 }
-                sb.append(node.num);
                 System.out.println(sb);
                 break;
             }
             if (node.num % 3 == 0 && !visited[node.num / 3]) {
-                q.add(new Node(node.count + 1, makeClone(node), node.num / 3));
+                q.add(new Node(node.count + 1, node.num / 3));
+                dp[node.num / 3] = node.num;
                 visited[node.num / 3] = true;
             }
             if (node.num % 2 == 0 && !visited[node.num / 2]) {
-                q.add(new Node(node.count + 1, makeClone(node), node.num / 2));
+                q.add(new Node(node.count + 1, node.num / 2));
+                dp[node.num / 2] = node.num;
                 visited[node.num / 2] = true;
             }
             if (!visited[node.num - 1]) {
-                q.add(new Node(node.count + 1, makeClone(node), node.num - 1));
+                q.add(new Node(node.count + 1, node.num - 1));
+                dp[node.num - 1] = node.num;
                 visited[node.num - 1] = true;
             }
         }
     }
 
-    static List<Integer> makeClone(Node node) {
-        List<Integer> list = new ArrayList<>(node.history);
-        list.add(node.num);
-        return list;
+    static void dfs(Stack<Integer> stack, int num) {
+        stack.push(num);
+        if (num == n) {
+            return;
+        }
+        dfs(stack, dp[num]);
     }
 
     static class Node implements Comparable<Node> {
         int count;
-        List<Integer> history;
         int num;
 
-        public Node(int count, List<Integer> history, int num) {
+        public Node(int count, int num) {
             this.count = count;
-            this.history = history;
             this.num = num;
         }
 
