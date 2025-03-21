ebf4fa624afe35d4eee3f81b38f0eea39f4273d2
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-03-21 14:43:43 +0900

    [level 3] Title: 주사위 고르기, Time: 745.31 ms, Memory: 160 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.java
@@ -1,93 +1,142 @@
 import java.util.*;
 class Solution {
-    static int n;
-    static int[] com;
-    static int[][] officialDice;
-    static int winCount = 0;
-    static int[] answerComb;
-
-    public static int[] solution(int[][] dice) {
-        n = dice.length;
-        com = new int[n/2];
-        answerComb = new int[n/2];
-        officialDice = dice;
-        getCombination(0, 0);
-        return answerComb;
+    int[] selectedDice;
+    int totalDice;
+    int[] answer;
+    int[][] dice;
+    int maxWinCount = 0;
+    
+    public int[] solution(int[][] dice) {
+        selectedDice = new int[dice.length / 2];
+        this.dice = dice;
+        totalDice = dice.length;
+        answer = new int[totalDice / 2];
+        
+        select(0, totalDice / 2, 0);
+        
+        return answer;
     }
-
-    public static void getCombination(int depth, int idx) {
-        if (depth == n / 2) {
-            int win = getWinningCount();
-            if (winCount < win) {
-                winCount = win;
-                for (int i = 0; i < n/ 2; i++) {
-                    answerComb[i] = com[i] + 1;
+    
+    void select(int d, int f, int now) {
+        if (d == f) {
+            int winCount = roll();
+            if (maxWinCount < winCount) {
+                maxWinCount = winCount;
+                for (int i = 0; i < totalDice / 2; i++) {
+                    answer[i] = selectedDice[i] + 1;
                 }
             }
             return;
         }
-        for (int i = idx; i < n; i++) {
-            com[depth] = i;
-            getCombination(depth + 1, i+ 1);
-            /**
-             * 0 1/ 0 2/0 3
-             *
-             */
+        for (int i = now; i < totalDice; i++) {
+            selectedDice[d] = i;
+            select(d + 1, f, i + 1);
         }
     }
-
-    public static int getWinningCount() {
-        List<Integer> listA = new ArrayList<>();
-        List<Integer> listB = new ArrayList<>();
-        int[][] comA = new int[n/2][6];
-        int[][] comB = new int[n/2][6];
-
-        int a = 0;
-        int b  = 0;
-        for (int i = 0; i < n; i++) {
-            boolean isContain = false;
-            for (int j = 0; j < n/2; j++) {
-                if (com[j] == i) {
-                    isContain = true;
-                    break;
+    
+    int roll() {
+        List<Integer> result1 = new ArrayList<>();
+        List<Integer> result2 = new ArrayList<>();
+        
+        // int[] result1 = new int[(int) Math.pow(6, totalDice / 2)];
+        // int[] result2 = new int[(int) Math.pow(6, totalDice / 2)];
+        
+        int[] nonSelectedDice = new int[selectedDice.length];
+        int idx = 0;
+        for (int i = 0; i < totalDice; i++) {
+            boolean isNotSelected = true;
+            for (int j = 0; j < selectedDice.length; j++) {
+                if (selectedDice[j] == i) {
+                    isNotSelected = false;
                 }
             }
-            if (isContain) {
-                comA[a++] = officialDice[i];
-            } else {
-                comB[b++] = officialDice[i];
+            if (isNotSelected) {
+                nonSelectedDice[idx++] = i;
             }
         }
-
-        makeList(listA, 0, 0, comA);
-        makeList(listB, 0, 0, comB);
-
-        Collections.sort(listB);
-        int count = 0;
-        for (int i = 0; i < listA.size(); i++) {
-            int left = 0;
-            int right = listB.size();
-            while (left < right) {
-                int mid = (left + right) / 2;
-                if (listB.get(mid) < listA.get(i)) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
-            }
-            count += right;
+        
+        getSum(0, result1, selectedDice, 0);
+        getSum(0, result2, nonSelectedDice, 0);
+        
+        
+//         idx = 0;
+//         for (int i = 0; i < selectedDice.length; i++) {
+//             for (int j = 0; j < 6; j++) {
+//                 for (int k = i + 1; k < selectedDice.length; k++) {
+//                     for (int t = 0; t < 6; t++) {
+//                         result1[idx++] = dice[selectedDice[i]][j] + dice[selectedDice[k]][t];
+//                     }
+//                 }
+//             }
+//         }
+        
+//         idx = 0;
+//         for (int i = 0; i < nonSelectedDice.length; i++) {
+//             for (int j = 0; j < 6; j++) {
+//                 for (int k = i + 1; k < nonSelectedDice.length; k++) {
+//                     for (int t = 0; t < 6; t++) {
+//                         result2[idx++] = dice[nonSelectedDice[i]][j] + dice[nonSelectedDice[k]][t];
+//                     }
+//                 }
+//             }
+//         }
+        
+//         if (selectedDice.length == 1) {
+//             for (int i = 0; i < 6; i++) {
+//                 result1[i] = dice[selectedDice[0]][i];
+//                 result2[i] = dice[nonSelectedDice[0]][i];
+//             }
+//         }
+        
+//         Arrays.sort(result1);
+//         Arrays.sort(result2);
+        
+        Collections.sort(result1);
+        Collections.sort(result2);
+        
+        int winCount = 0;
+        for (int i = 0; i < result1.size(); i++) {
+            winCount += search(result1.get(i), result2);
         }
-        return count;
+//         while (idx1 < result1.length) {
+//             winCount += search(result1[idx1], result2);
+//             idx1++;
+//         }
+        
+        return winCount;
+        
     }
-
-    public static void makeList(List<Integer> list, int depth, int sum, int[][] dice) {
-        if (depth == n / 2) {
-            list.add(sum);
+    
+    void getSum(int d, List<Integer> result, int[] selected, int value) {
+        if (d == selected.length) {
+            result.add(value);
             return;
         }
+        
         for (int i = 0; i < 6; i++) {
-            makeList(list, depth + 1, sum + dice[depth][i], dice);
+            getSum(d + 1, result, selected, value + dice[selected[d]][i]);
         }
+    }
+    
+    int search(int number, List<Integer> arr) {
+        int left = 0;
+        int right = arr.size();
+        while (left < right) {
+            int mid = (left + right) / 2;
+            // if (arr[mid] == number) {
+            //     while (0 <= mid && arr[mid] == number) {
+            //         mid--;
+            //     }
+            //     return mid + 1;
+            // }
+            if (arr.get(mid) < number) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
+            }
+        }
+        return right;
     }
 
 }
