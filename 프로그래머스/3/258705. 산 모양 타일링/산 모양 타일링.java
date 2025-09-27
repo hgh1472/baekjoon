6f9f648ca0a1bece2c3b996eb88b8cda1f53c45c
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-09-27 11:02:39 +0900

    [level 3] Title: 산 모양 타일링, Time: 4.15 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,14 +1,30 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[] tops) {
-        int[] connectedDp = new int[n];
-        int[] separateDp = new int[n];
-        connectedDp[0] = 1;
-        separateDp[0] = 2 + tops[0];
-        for (int i = 1; i < n; i++) {
-            connectedDp[i] = (connectedDp[i-1] + separateDp[i-1]) % 10007;
-            separateDp[i] = ((1 + tops[i]) * connectedDp[i-1] + (2  +tops[i]) * separateDp[i-1]) % 10007;
+        int answer = 0;
+        int a, b;
+        if (tops[0] == 1) {
+            a = 3;
+            b = 1;
         }
-        return (connectedDp[n-1] + separateDp[n-1]) % 10007;
+        else {
+            a = 2;
+            b = 1;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            int newA = a;
+            int newB = b;
+            if (tops[i] == 1) {
+                newA = a * 3 + b * 2;
+            }
+            else {
+                newA = a * 2 + b;
+            }
+            newB = a + b;
+            a = newA % 10007;
+            b = newB % 10007;
+        }
+        
+        return (a + b) % 10007;
     }
 }
