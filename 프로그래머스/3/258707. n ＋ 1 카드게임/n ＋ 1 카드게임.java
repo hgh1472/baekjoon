5ad46955e44e270c02db92e70a290d80b4bb638c
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-09-27 10:26:08 +0900

    [level 3] Title: n + 1 카드게임, Time: 3.82 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -1,75 +1,59 @@
-import java.util.*;
 class Solution {
     public int solution(int coin, int[] cards) {
+        boolean[] have = new boolean[cards.length+1];
+        boolean[] draw = new boolean[cards.length+1];
         int n = cards.length;
-        HashMap<Integer, Boolean> containCard = new HashMap<>();
-        int[] pairRound = new int[n + 1];
-        boolean[] isDiscard = new boolean[n+1];
-        for (int i = 0; i < n; i++) {
-            if (i < n/3) {
-                containCard.put(cards[i], true);
-                pairRound[(n+1) - cards[i]] = 0;
-            }
-            else {
-                pairRound[(n+1) - cards[i]] = (i - n/3) / 2 + 1;
-            }
+        for (int i = 0; i < n / 3; i++) {
+            have[cards[i]] = true;
         }
-
-        int round = 1;
-        int possibleRound = 0;
-        // 처음에 뽑은 카드 중 쌍이 있는지 확인
-        List<Integer> removeCard = new ArrayList<>();
-        for (Integer key : containCard.keySet()) {
-            if (containCard.containsKey((n + 1) - key)) {
-                removeCard.add(key);
-                possibleRound++;
+        
+        int round;
+        for (round = 0; round < n / 3; round++) {
+            draw[cards[n/3 + 2*round]] = true;
+            draw[cards[n/3 + 2*round + 1]] = true;
+            boolean next = false;
+            
+            for (int i = 1; i <= n; i++) {
+                if (have[i] && have[n+1-i]) {
+                    have[i] = false;
+                    have[n+1-i] = false;
+                    next = true;
+                    break;
+                }
             }
-        }
-        possibleRound = 1 + possibleRound / 2;
-        for (Integer key : removeCard) {
-            containCard.remove(key);
-            isDiscard[key] = true;
-        }
-
-        while (round <= possibleRound && coin > 0) {
-            if (n/3 + possibleRound * 2 > n)
+            if (next) continue;
+            
+            if (coin == 0) {
                 break;
-            boolean draw = false;
-            // 소유하고 있는 카드 중 짝을 뽑을 수 있는 경우
-            if (coin >= 1) {
-                removeCard.clear();
-                for (Integer key : containCard.keySet()) {
-                    if (pairRound[key] <= possibleRound) {
-                        removeCard.add(key);
-                        coin--;
-                        possibleRound++;
-                        draw = true;
-                        isDiscard[key] = true;
-                        isDiscard[(n+1) - key] = true;
-                        break;
-                    }
+            }
+            for (int i = 1; i <= n; i++) {
+                if (have[i] && draw[n+1-i]) {
+                    have[i] = false;
+                    draw[n+1-i] = false;
+                    next = true;
+                    coin--;
+                    break;
                 }
             }
-            for (Integer key : removeCard) {
-                containCard.remove(key);
+            if (next) continue;
+            
+            if (coin < 2) {
+                break;
             }
-            // 2개를 뽑아야 하는 경우 = 갖고있는 카드로 뽑을 수 있는게 없다.
-            // 2개 뽑는 것과 1개 뽑는 것중 1개 뽑는게 우선이어야 한다.
-            if (!draw && coin >= 2) {
-                for (int i = n/3; i < n/3 + (possibleRound) * 2; i++) {
-                    if (isDiscard[cards[i]])
-                        continue;
-                    if (pairRound[cards[i]] <= possibleRound && !isDiscard[(n+1) - cards[i]]) {
-                        coin -= 2;
-                        isDiscard[cards[i]] = true;
-                        isDiscard[(n+1) - cards[i]] = true;
-                        possibleRound++;
-                        break;
-                    }
+            for (int i = 1; i <= n; i++) {
+                if (draw[i] && draw[n+1-i]) {
+                    draw[i] = false;
+                    draw[n+1-i] = false;
+                    next = true;
+                    coin -= 2;
+                    break;
                 }
             }
-            round++;
+            if (!next) {
+                break;
+            }
         }
-        return possibleRound;
+        
+        return round + 1;
     }
 }
