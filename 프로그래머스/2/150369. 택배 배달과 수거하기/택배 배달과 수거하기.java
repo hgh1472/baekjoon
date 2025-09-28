4588f034b50a637b51e11d1eb126f6f365cc41b8
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-09-28 11:23:16 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 76.55 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,17 +1,70 @@
+import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long distance = 0L;
-        int carry = 0;
-        int get = 0;
-        for (int i = n; i > 0; i--) {
-            carry += deliveries[i - 1];
-            get += pickups[i - 1];
-            while (carry > 0 || get > 0) {
-                carry -= cap;
-                get -= cap;
-                distance += i * 2;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        PriorityQueue<Node> dq = new PriorityQueue<>();
+        for (int i = 0; i < deliveries.length; i++) {
+            if (deliveries[i] == 0) {
+                continue;
             }
+            dq.add(new Node(i+1, deliveries[i]));
         }
-        return distance;
+        for (int i = 0; i < pickups.length; i++) {
+            if (pickups[i] == 0) {
+                continue;
+            }
+            pq.add(new Node(i+1, pickups[i]));
+        }
+        
+        long answer = 0;
+        while (!dq.isEmpty() || !pq.isEmpty()) {
+            int max = 0;
+            int pickup = cap;
+            int delivery = cap;
+            while (delivery != 0 && !dq.isEmpty()) {
+                Node peek = dq.peek();
+                max = Math.max(max, peek.number);
+                if (peek.count > delivery) {
+                    peek.count -= delivery;
+                    delivery = 0;
+                }
+                else {
+                    delivery -= peek.count;
+                    dq.poll();
+                }
+            }
+            
+            while (pickup != 0 && !pq.isEmpty()) {
+                Node peek = pq.peek();
+                max = Math.max(max, peek.number);
+                if (peek.count > pickup) {
+                    peek.count -= pickup;
+                    pickup = 0;
+                }
+                else {
+                    pickup -= peek.count;
+                    pq.poll();
+                }
+            }
+            
+            answer += max * 2;
+        }
+        return answer;
+    }
+    
+    class Node implements Comparable<Node> {
+        int number;
+        int count;
+        
+        Node(int number, int count) {
+            this.number = number;
+            this.count = count;
+        }
+        
+        @Override
+        public int compareTo(Node n) {
+            return n.number - this.number;
+        }
     }
 }
