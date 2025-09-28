f0e152e70442a7003987e98a3ace388d159498fb
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-09-28 10:46:34 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.10 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 78.3 MB, 시간: 3.02 ms
+메모리: 86.4 MB, 시간: 1.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 05일 09:58:32
+2025년 09월 28일 10:46:31
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,52 +1,34 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> temp = new ArrayList<>();
-        
-        String[] a = today.split("\\.");
-        int[] todayInfo = new int[3];
-        for (int i = 0; i < 3; i++)
-            todayInfo[i] = Integer.parseInt(a[i]);
-        for (int i = 0; i < privacies.length; i++) {
-            String[] dayAndTerm = privacies[i].split(" ");
-            if (isExpired(todayInfo, dayAndTerm[0], calculateTerm(terms, dayAndTerm[1])))
-                temp.add(i + 1);
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < terms.length; i++) {
+            String[] info = terms[i].split(" ");
+            map.put(info[0], Integer.parseInt(info[1]));
         }
-        int[] answer = new int[temp.size()];
-        for (int i = 0; i < temp.size(); i++)
-            answer[i] = temp.get(i);
+        
+        int todayNum = calculate(today);
+        
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < privacies.length; i++) {
+            String[] info = privacies[i].split(" ");
+            int num = calculate(info[0]);
+            int add = 28 * map.get(info[1]);
+            if (todayNum >= num + add) {
+                result.add(i);
+            }
+        }
+        
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i) + 1;
+        }
         return answer;
     }
     
-    public boolean isExpired(int[] todayInfo, String contractDay, int term) {
-        String[] b = contractDay.split("\\.");
-        int[] contractDayInfo = new int[3];
-        for (int i = 0; i < 3; i++)
-            contractDayInfo[i] = Integer.parseInt(b[i]);
-        
-        contractDayInfo[1] += term;
-        if (contractDayInfo[1] >= 13) {
-            while (contractDayInfo[1] >= 13) {
-                contractDayInfo[1] -= 12;
-                contractDayInfo[0] += 1;
-            }
-        }
-        for (int i = 0; i < 3; i++) {
-            if (todayInfo[i] < contractDayInfo[i])
-                return false;
-            else if (todayInfo[i] > contractDayInfo[i])
-                return true;
-        }
-        return true;
-    }
-    
-    public int calculateTerm(String[] terms, String contract) {
-        for (String s : terms) {
-            String[] info = s.split(" ");
-            if (info[0].equals(contract)) {
-                return Integer.parseInt(info[1]);
-            }
-        }
-        return 0;
+    int calculate(String day) {
+        String[] info = day.split("\\.");
+        return 28 * 12 * Integer.parseInt(info[0]) + 28 * Integer.parseInt(info[1]) + Integer.parseInt(info[2]);
     }
 }
