1854d1e4328a9a05f9ec836c98f4d7f1e47911c7
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-09-29 23:16:29 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 28.90 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -3,53 +3,47 @@ import java.util.*;
 class Solution {
     public int[] solution(long[] numbers) {
         int[] answer = new int[numbers.length];
-        int idx = 0;
-        for (long number : numbers) {
-            String binary = calculateBinary(number);
-            int lastDepth = 0;
-            while (!(Math.pow(2, lastDepth) <= binary.length() && binary.length() < Math.pow(2, lastDepth + 1)))
-                lastDepth++;
-            boolean result = dfs(binary, 0, binary.length() - 1, 0, lastDepth, false);
-            if (result)
-                answer[idx++] = 1;
-            else
-                answer[idx++] = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            String binary = convert(numbers[i]);
+            answer[i] = check(binary, 0, binary.length() - 1, true);
         }
-        
         return answer;
     }
     
-    public boolean dfs(String binary, int left, int right, int depth, int lastDepth, boolean isZero) {
-        if (depth > lastDepth)
-            return true;
-        int mid = (left + right) / 2;
-        if (binary.charAt(mid) == '1' && isZero) {
-            return false;
+    int check(String binary, int left, int right, boolean possible) {
+        if (left > right) {
+            return 1;
         }
-        boolean next = binary.charAt(mid) == '0';
-        if (!dfs(binary, left, mid - 1, depth + 1, lastDepth, next))
-            return false;
-        return dfs(binary, mid + 1, right, depth + 1, lastDepth, next);
+        int mid = (left + right) / 2;
+        boolean isOne = binary.charAt(mid) == '1';
+        if (!possible && isOne) {
+            return 0;
+        }
+        if (check(binary, left, mid - 1, isOne) == 0) {
+            return 0;
+        }
+        return check(binary, mid + 1, right, isOne);
     }
     
-    public String calculateBinary(long number) {
+    String convert(long number) {
         StringBuilder sb = new StringBuilder();
-        while (number != 0) {
+        while (number >= 2) {
             sb.append(number % 2);
             number /= 2;
         }
-        fillDummy(sb);
-        return sb.reverse().toString();
+        sb.append(number);
+        String add = add(sb.length());
+        return sb.append(add).reverse().toString();
     }
     
-    public void fillDummy(StringBuilder sb) {
-        int length = sb.length();
-        int count = 0;
-        while (!(Math.pow(2, count) <= length && length < Math.pow(2, count + 1))) {
-            count += 1;
+    String add(int length) {
+        if (length == 1) {
+            return "";
         }
-        for (int i = length; i < Math.pow(2, count + 1) - 1; i++) {
-            sb.append("0");
+        int len = 0;
+        while ((int) Math.pow(2, len) <= length) {
+            len++;
         }
+        return "0".repeat(((int) Math.pow(2, len)) - 1 - length);
     }
 }
