6674bec2d6242035d1b7e7651f36a84deda37eb3
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-10-02 16:36:28 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 14.61 ms, Memory: 54.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,48 +1,60 @@
 class Solution {
-    static int alpDst = 0;
-    static int copDst = 0;
-    static int[][] dp;
     public int solution(int alp, int cop, int[][] problems) {
-        for (int[] problem : problems) {
-            if (alpDst < problem[0])
-                alpDst = problem[0];
-            if (copDst < problem[1])
-                copDst = problem[1];
+        int maxAlp = alp, maxCop = cop;
+        for (int i = 0; i < problems.length; i++) {
+            maxAlp = Math.max(maxAlp, problems[i][0]);
+            maxCop = Math.max(maxCop, problems[i][1]);
         }
-        if (cop >= copDst && alp >= alpDst)
-            return 0;
-        if (cop >= copDst)
-            cop = copDst;
-        if (alp >= alpDst)
-            alp = alpDst;
-        dp = new int[alpDst+1][copDst+1];
-        for (int i = alp; i <= alpDst; i++) {
-            for (int j = cop; j <= copDst; j++) {
-                dp[i][j] = 2000000000;
+        
+        int[][] dp = new int[maxAlp + 2][maxCop + 2];
+        for (int i = alp; i <= maxAlp; i++) {
+            for (int j = cop; j <= maxCop; j++) {
+                dp[i][j] = 500;
             }
         }
         dp[alp][cop] = 0;
         
-        for (int i = alp; i <= alpDst; i++) {
-            for (int j = cop; j <= copDst; j++) {
-                if (j + 1 <= copDst)
-                    dp[i][j + 1] = Math.min(dp[i][j] + 1, dp[i][j+1]);
-                if (i+1 <= alpDst)
-                    dp[i+1][j] = Math.min(dp[i][j] + 1, dp[i+1][j]);
-                for (int[] p : problems) {
-                    if (i >= p[0] && j >= p[1]) { // 문제 풀 수 있음
-                        if (i + p[2] <= alpDst && j + p[3] <= copDst) // 목표치 내
-                            dp[i+p[2]][j+p[3]] = Math.min(dp[i][j]+p[4], dp[i+p[2]][j+p[3]]);
-                        else if (i + p[2] <= alpDst && j+p[3] > copDst) // j가 초과
-                            dp[i+p[2]][copDst] = Math.min(dp[i][j]+p[4],dp[i+p[2]][copDst]);
-                        else if (i + p[2] > alpDst && j+p[3] <= copDst)
-                            dp[alpDst][j+p[3]] = Math.min(dp[i][j]+p[4], dp[alpDst][j+p[3]]);
-                        else
-                            dp[alpDst][copDst] = Math.min(dp[i][j]+p[4], dp[alpDst][copDst]);
+        for (int i = alp; i <= maxAlp; i++) {
+            for (int j = cop; j <= maxCop; j++) {
+                if (dp[i][j] == 500) {
+                    continue;
+                }
+                dp[i+1][j] = Math.min(dp[i][j] + 1, dp[i+1][j]);
+                dp[i][j+1] = Math.min(dp[i][j] + 1, dp[i][j+1]);
+                
+                for (int[] problem : problems) {
+                    if (problem[0] > i || problem[1] > j) {
+                        continue;
                     }
+                    int nextAlp = Math.min(maxAlp, i + problem[2]);
+                    int nextCop = Math.min(maxCop, j + problem[3]);
+                    dp[nextAlp][nextCop] = Math.min(dp[nextAlp][nextCop], dp[i][j] + problem[4]);
                 }
             }
         }
-        return dp[alpDst][copDst];
+        
+        return dp[maxAlp][maxCop];
     }
+    /**
+    목표: 최단 시간으로 (max alp, max cop) 달성
+    완탐: (102)^100 -> 시간 초과
+    dp[i][j] = (i, j)를 갖추는데 필요한 최소 시간
+    int[maxAlp][maxCop]
+    i = alp ~ maxAlp
+      j = cop ~ maxCop
+        if (dp[i][j] == 500) {
+          continue;
+        }
+        dp[i+1][j] = Math.min(dp[i][j] + 1, dp[i+1][j]);
+        dp[i][j+1] = Math.min(dp[i][j] + 1, dp[i][j+1]);
+        
+        for (int[] problem : problems) {
+          if (풀수있음) {
+            nextAlp = Math.min(maxAlp, i + 보상알고력);
+            nextCop = Math.min(maxCop, j + 보상코딩력);
+            dp[nextAlp][nextCop] = dp[i][j] + 문제소요시간;
+          }
+        }
+        return dp[maxAlp][maxCop];
+    */
 }
