eebb2a03dc62f26647bb6c156c8a1ee0bec72f2d
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-10-03 10:24:56 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 45.43 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,61 +1,44 @@
 import java.util.*;
 
 class Solution {
-    static int[] q1 = new int[600001];
-    static int[] q2 = new int[600001];
-    static int sum1 = 0;
-    static int sum2 = 0;
-    static int front1 = 0;
-    static int rear1 = 0;
-    static int front2 = 0;
-    static int rear2 = 0;
     public int solution(int[] queue1, int[] queue2) {
-        CustomQueue q1 = new CustomQueue(queue1);
-        CustomQueue q2 = new CustomQueue(queue2);
+        long sum1 = 0;
+        long sum2 = 0;
         
-        for (int i = 0; i < 600005; i++) {
-            if (q1.sum == q2.sum) {
-                return i;
+        Deque<Integer> q1 = new ArrayDeque<>();
+        Deque<Integer> q2 = new ArrayDeque<>();
+        for (int i = 0; i < queue1.length; i++) {
+            sum1 += (long) queue1[i];
+            q1.addLast(queue1[i]);
+        }
+        for (int i = 0; i < queue2.length; i++) {
+            sum2 += (long) queue2[i];
+            q2.addLast(queue2[i]);
+        }
+        
+        int count = 0;
+        while (sum1 != sum2 && count <= queue1.length * 3) {
+            if (sum1 == sum2) {
+                return count;
             }
-            else if (q1.sum > q2.sum) {
-                int e = q1.pop();
-                q2.push(e);
+            else if (sum1 > sum2) {
+                int num = q1.removeFirst();
+                q2.addLast(num);
+                sum1 -= num;
+                sum2 += num;
             }
-            else {
-                int e = q2.pop();
-                q1.push(e);
+            else if (sum1 < sum2) {
+                int num = q2.removeFirst();
+                q1.addLast(num);
+                sum2 -= num;
+                sum1 += num;
             }
+            count++;
+        }
+        
+        if (sum1 == sum2) {
+            return count;
         }
         return -1;
-    }
-    
-    class CustomQueue {
-        int front;
-        int rear;
-        long sum;
-        int[] queue;
-        
-        public CustomQueue(int[] queue) {
-            this.front = 0;
-            this.rear = 0;
-            this.sum = 0L;
-            this.queue = new int[600001];
-            for (int i = 0; i < queue.length; i++) {
-                push(queue[i]);
-            }
-        }
-        
-        public int pop() {
-            int e = queue[this.rear];
-            this.sum -= e;
-            this.rear = (this.rear + 1) % 600001;
-            return e;
-        }
-        
-        public void push(int e) {
-            queue[this.front] = e;
-            this.sum += e;
-            this.front = (this.front + 1) % 600001;
-        }
     }
 }
