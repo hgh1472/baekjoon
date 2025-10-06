f6b38742521583994e7bee498ea353a47f825e92
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-10-06 21:17:31 +0900

    [level 3] Title: 양과 늑대, Time: 1.54 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,36 +1,28 @@
-import java.util.*;
-
 class Solution {
-    static int[][] edgeInfo;
-    static int[] nodeInfo;
-    static boolean[] visited;
-    static int answer = 1;
+    int answer;
     public int solution(int[] info, int[][] edges) {
-        edgeInfo = edges;
-        nodeInfo = info;
-        visited = new boolean[info.length];
+        boolean[] visited = new boolean[info.length];
         visited[0] = true;
-        dfs(1, 0);
+        dfs(info, edges, visited, 1, 0);
         return answer;
     }
     
-    public void dfs(int sheep, int wolf) {
+    public void dfs(int[] info, int[][] edges, boolean[] visited, int sheep, int wolf) {
         if (sheep <= wolf) {
             return;
         }
-        answer = Math.max(answer, sheep);
-        for (int[] edge : edgeInfo) {
+        answer = Math.max(sheep, answer);
+        for (int[] edge : edges) {
             if (visited[edge[0]] && !visited[edge[1]]) {
                 visited[edge[1]] = true;
-                if (nodeInfo[edge[1]] == 0) {
-                    dfs(sheep + 1, wolf);
+                if (info[edge[1]] == 0) {
+                    dfs(info, edges, visited, sheep + 1, wolf);
                 }
                 else {
-                    dfs(sheep, wolf + 1);
+                    dfs(info, edges, visited, sheep, wolf + 1);
                 }
                 visited[edge[1]] = false;
             }
         }
     }
-    
 }
