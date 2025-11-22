ac981baaacab200efb8d4861402090d986086b2b
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2025-11-22 22:24:41 +0900

    [Gold II] Title: 우수 마을, Time: 264 ms, Memory: 26668 KB -BaekjoonHub

---
+++ 백준/Gold/1949. 우수 마을/우수 마을.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Main {
+    static boolean[] visited;
+    static int n;
+    static int[] people;
+    static int[][] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n];
+        people = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Node[] nodes = new Node[n];
+        dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node(i);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            nodes[input[0] - 1].subs.add(nodes[input[1] - 1]);
+            nodes[input[1] - 1].subs.add(nodes[input[0] - 1]);
+        }
+
+        visited[0] = true;
+        int answer = dfs(nodes[0], false);
+        System.out.println(answer);
+    }
+
+    static int dfs(Node node, boolean impossible) {
+        int result1 = 0;
+
+        // 우수 마을이 되지 않는 경우
+        if (dp[node.number][0] == -1) {
+            for (Node sub : node.subs) {
+                if (visited[sub.number]) {
+                    continue;
+                }
+                visited[sub.number] = true;
+                result1 += dfs(sub, false);
+                visited[sub.number] = false;
+            }
+            dp[node.number][0] = result1;
+        }
+        result1 = dp[node.number][0];
+        if (impossible) {
+            return result1;
+        }
+
+        // 우수 마을이 될 수 있는 경우
+        int result2 = people[node.number];
+        if (dp[node.number][1] == -1) {
+            for (Node sub : node.subs) {
+                if (visited[sub.number]) {
+                    continue;
+                }
+                visited[sub.number] = true;
+                result2 += dfs(sub, true);
+                visited[sub.number] = false;
+            }
+            dp[node.number][1] = result2;
+        }
+        result2 = dp[node.number][1];
+        return Math.max(result1, result2);
+    }
+
+    static class Node {
+        int number;
+        List<Node> subs = new ArrayList<>();
+
+        public Node(int number) {
+            this.number = number;
+        }
+    }
+}
