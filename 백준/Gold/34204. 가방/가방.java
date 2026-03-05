b6dd2280ce63091a843c5d34b02a9b07cc2f51a8
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2026-03-05 22:48:51 +0900

    [Gold IV] Title: 가방, Time: 384 ms, Memory: 43664 KB, Score: 69 point -BaekjoonHub

---
+++ 백준/Gold/34204. 가방/가방.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, k, c;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        n = input[0];
+        k = input[1];
+        c = input[2];
+        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(numbers);
+
+        int left = 0;
+        int right = Math.min(k, n);
+        int steel = 0;
+        for (int i = left; i < right; i++) {
+            steel += numbers[i];
+        }
+
+        int weight = 0, idx = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= c; i++) {
+            if (i - weight >= numbers[idx]) {
+                if (right < n) {
+                    steel += numbers[right];
+                    right++;
+                    steel -= numbers[left];
+                    weight += numbers[left];
+                    left++;
+                    idx++;
+                }
+            }
+            sb.append(steel).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
